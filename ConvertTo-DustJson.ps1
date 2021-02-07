ad7fb27971bcfd4da27027d64be97cf2e5021bd5
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-02-07 23:44:02 +0100

    Update

--- ConvertTo-DustJson.ps1
+++ ConvertTo-DustJson.ps1
@@ -3,11 +3,11 @@ param (
     $obj,
 
     [Parameter()]
-    $depth = 20
+    [int]$depth = 20
 )
 
 $obj.PsObject.Properties | ForEach-Object {
-    if($_.Value -is [System.DateTime]) {
+    if ($_.Value -is [System.DateTime]) {
         $obj."$($_.Name)" = Get-Date $_.Value -Format o
     } elseif ($_.Value -is [System.Int64] -and $_.Value -gt 94354812000000000) { # Greater than 01.01.1900
         $obj."$($_.Name)" = Get-Date ([DateTime]::FromFileTime($_.Value)) -Format o
