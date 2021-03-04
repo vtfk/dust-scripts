df901df06c76e8ed086e45a65700c94ea156580b
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-03-04 14:45:12 +0100

    Lagt til støtte for å søke på flere properties Lagt til flere properties som hentes ut Output blir sortert etter property name

--- Get-DUSTFeide.ps1
+++ Get-DUSTFeide.ps1
@@ -1,17 +1,38 @@
 param(
-    [Parameter()]
+    [Parameter(ParameterSetName = "SAM")]
     [string]$SamAccountName,
 
+    [Parameter(ParameterSetName = "UPN")]
+    [string]$UserPrincipalName,
+
+    [Parameter(ParameterSetName = "SSN")]
+    [string]$EmployeeNumber,
+
+    [Parameter(ParameterSetName = "DisplayName")]
+    [string]$DisplayName,
+
     [Parameter()]
-    [string[]]$Properties = @('displayName', 'distinguishedName', 'enabled', 'passwordLastSet', 'lastLogonDate', 'whenChanged', 'whenCreated', 'eduPersonAffiliation', 'eduPersonEntitlement', 'eduPersonOrgUnitDN', 'norEduPersonAuthnMethod')
+    [string[]]$Properties = @('displayName', 'distinguishedName', 'enabled', 'passwordLastSet', 'lastLogonDate', 'whenChanged', 'whenCreated', 'eduPersonAffiliation', 'eduPersonEntitlement', 'eduPersonOrgUnitDN', 'norEduPersonAuthnMethod', 'norEduPersonNIN')
 )
 
-if (!$SamAccountName) {
-    Write-Error -Message "Missing required parameter: 'SamAccountName' !" -ErrorAction Stop
+if ($SamAccountName) {
+    $filter = "Name -eq '$SamAccountName'"
+}
+elseif ($UserPrincipalName) {
+    $filter = "mail -eq '$UserPrincipalName'"
+}
+elseif ($EmployeeNumber) {
+    $filter = "norEduPersonNIN -eq '$EmployeeNumber'"
+}
+elseif ($DisplayName) {
+    $filter = "DisplayName -like '$DisplayName'"
+}
+else {
+    Write-Error -Message "Missing required parameter: 'SamAccountName' or 'UserPrincipalName' or 'EmployeeNumber' or 'DisplayName' !" -ErrorAction Stop
 }
 
 # default properties that must be present!
-@('distinguishedName', 'enabled', 'GivenName', 'Name', 'Surname', 'UserPrincipalName', 'displayName', 'employeeNumber', 'company', 'department', 'passwordLastSet', 'whenChanged', 'whenCreated', 'eduPersonAffiliation', 'eduPersonEntitlement', 'eduPersonOrgUnitDN', 'norEduPersonAuthnMethod') | ForEach-Object {
+@('distinguishedName', 'enabled', 'GivenName', 'Name', 'Surname', 'mail', 'displayName', 'norEduPersonNIN', 'passwordLastSet', 'whenChanged', 'whenCreated', 'eduPersonAffiliation', 'eduPersonEntitlement', 'eduPersonOrgUnitDN', 'norEduPersonAuthnMethod', 'norEduPersonNIN') | ForEach-Object {
     if (!$Properties.ToLower().Contains($_.ToLower())) {
         $Properties += $_
     }
@@ -21,7 +42,7 @@ if (!$SamAccountName) {
 $envPath = Join-Path -Path $PSScriptRoot -ChildPath "envs.ps1"
 . $envPath
 
-$user = Get-ADUser -Filter { Name -eq $SamAccountName } -Server $feide.server -SearchBase $feide.searchBase -Properties $Properties | Select-Object $Properties
+$user = Get-ADUser -Filter $filter -Server $feide.server -SearchBase $feide.searchBase -Properties $Properties | Select-Object ($Properties | Sort-Object)
 if(!$user) {
     # No user was found! :(
     return "[]"
