d2994489f9fc3a7f38d1854c817bfada77efd021
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-03-18 15:41:36 +0100

    Håndtere oppdatert PIFU

--- node/get-dust-pifu.js
+++ node/get-dust-pifu.js
@@ -4,10 +4,10 @@ const pifu = require(process.env.PIFU_FILE_PATH)
 const employeeNumber = process.argv.slice(2)[0]
 
 // find person
-const person = pifu.enterprise.person.find(p => p.sourcedid.id === Number.parseInt(employeeNumber))
+const person = pifu.enterprise.person.find(p => p.sourcedid.id === employeeNumber)
 
 // find memberships
-const id = person ? person.sourcedid.id : Number.parseInt(employeeNumber)
+const id = person ? person.sourcedid.id : employeeNumber
 const memberships = pifu.enterprise.membership.filter(membership => membership && Array.isArray(membership.member) && !!membership.member.find(member => member.sourcedid && member.sourcedid.id === id))
   .map(ships => {
     ships.member = ships.member.find(member => member.sourcedid && member.sourcedid.id === id)
