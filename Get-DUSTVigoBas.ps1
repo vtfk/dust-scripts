acd5724305244ae7a2e7431b8613be32cef530ab
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-04-07 16:31:08 +0200

    La til VigoBas-skript

---
+++ Get-DUSTVigoBas.ps1
@@ -0,0 +1,16 @@
+﻿param(
+    [Parameter()]
+    [string]$TaskName = "FullRunVigoBas"
+)
+
+# import environment variables
+$envPath = Join-Path -Path $PSScriptRoot -ChildPath "envs.ps1"
+. $envPath
+
+$lastRunDate = Invoke-Command -ComputerName $vigoBas.server -ScriptBlock { Get-ChildItem -Path C:\Windows\Logs\AutoRun -Directory | Select-Object -First 1 } | Select-Object @{N="lastWriteTime"; E={$_.LastWriteTime}} | .\Fix-Properties.ps1 | ConvertTo-Json
+
+if (!$lastRunDate) {
+    return "[]"
+}
+
+$lastRunDate
