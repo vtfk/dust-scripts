36bd1721c1b3ce6cba5f120315f15cc2cc658512
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-04-13 11:31:23 +0200

    Bruker shared folder istedenfor Invoke-Command

--- Get-DUSTVigoBas.ps1
+++ Get-DUSTVigoBas.ps1
@@ -7,7 +7,7 @@
 $envPath = Join-Path -Path $PSScriptRoot -ChildPath "envs.ps1"
 . $envPath
 
-$lastRunDate = Invoke-Command -ComputerName $vigoBas.server -ScriptBlock { Get-ChildItem -Path C:\Windows\Logs\AutoRun -Directory | Select-Object -First 1 } | Select-Object @{N="lastWriteTime"; E={$_.LastWriteTime}} | .\Fix-Properties.ps1 | ConvertTo-Json
+$lastRunDate = Get-ChildItem -Path $vigoBas.autoRun -Directory | Select-Object -First 1 | Select-Object @{N="lastWriteTime"; E={$_.LastWriteTime}} | .\Fix-Properties.ps1 | ConvertTo-Json
 
 if (!$lastRunDate) {
     return "[]"
