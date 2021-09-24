5d4f15bce33c36ea6050ece626338eb34b1b77fc
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-09-24 09:32:21 +0200

    Use correct variable

--- Get-DUSTVigoBas.ps1
+++ Get-DUSTVigoBas.ps1
@@ -2,7 +2,7 @@
 $envPath = Join-Path -Path $PSScriptRoot -ChildPath "envs.ps1"
 . $envPath
 
-$lastRunDate = Get-ChildItem -Path $idm.autoRun -File -Filter $idm.lastRun | Select-Object @{N="lastRunTime"; E={$_.LastWriteTime}} | .\Fix-Properties.ps1 | ConvertTo-Json
+$lastRunDate = Get-ChildItem -Path $idm.autoRun -File -Filter $idm.file | Select-Object @{N="lastRunTime"; E={$_.LastWriteTime}} | .\Fix-Properties.ps1 | ConvertTo-Json
 
 if (!$lastRunDate) {
     return "[]"
