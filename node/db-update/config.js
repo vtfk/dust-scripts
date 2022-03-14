ce5f8d28ccfd38243f11d99406e38bb9ed5f94b2
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-03-14 09:25:16 +0100

    Support to be called manually aswell as summoned

--- node/db-update/config.js
+++ node/db-update/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: '../.env' })
+const { join } = require('path')
+require('dotenv').config({ path: join(__dirname, '../.env') })
 
 module.exports = {
   MONGODB_CONNECTION: process.env.MONGODB_CONNECTION || false,
