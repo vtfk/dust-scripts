14714619c4ca51c716bfc11e4f0c02668c3adf34
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-06-17 15:04:00 +0200

    Added `type` to Elev, OT and Lærling Lærling is now in the same OU as Elev

--- DB/Update-DUSTDbUsers.ps1
+++ DB/Update-DUSTDbUsers.ps1
@@ -1,46 +1,63 @@
-﻿$adUsers = @()
-Write-Host "Starting : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+Function Update-DUSTADUsers {
+    $adUsers = @()
+    Write-Host "Starting AD Users : $(Get-Date -Format 'HH:mm:ss')" -Verbose
 
-# get ad users from AUTO USERS
-Write-Host "Finding 'AUTO USERS' from login.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "login.top.no" -Filter "*" -Properties memberOf,givenName,sn,displayName,employeeNumber,extensionAttribute4,extensionAttribute6,extensionAttribute7,company,physicalDeliveryOfficeName,mail,proxyAddresses,title,state -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"login"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, extensionAttribute7, @{N="departmentShort"; E={$_.extensionAttribute6}}, @{N="departmentAdditional"; E={$_.extensionAttribute4}}, @{N="office"; E={$_.physicalDeliveryOfficeName}}, @{N="company"; E={$_.company}}, title, state, @{N="feide"; E={if ($_.memberOf -like "*VT-ALLE-LÆRERE*") {$True} else {$False}}}
-Write-Host "Finding 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
-Write-Host "Finding apprentice 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS APPRENTICE" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
-Write-Host "Finding OT 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS OT" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
+    # get ad users from AUTO USERS
+    Write-Host "Finding 'AUTO USERS' from login.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "login.top.no" -Filter "*" -Properties memberOf,givenName,sn,displayName,employeeNumber,extensionAttribute4,extensionAttribute6,extensionAttribute7,company,physicalDeliveryOfficeName,mail,proxyAddresses,title,state -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"login"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, extensionAttribute7, @{N="departmentShort"; E={$_.extensionAttribute6}}, @{N="departmentAdditional"; E={$_.extensionAttribute4}}, @{N="office"; E={$_.physicalDeliveryOfficeName}}, @{N="company"; E={$_.company}}, title, state, @{N="feide"; E={if ($_.memberOf -like "*VT-ALLE-LÆRERE*") {$True} else {$False}}}
+    Write-Host "Finding 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "Title -eq 'Elev'" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses,title -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={$_.title.ToLower()}}
 
-# get ad users from AUTO DISABLED USERS
-Write-Host "Finding 'AUTO DISABLED USERS' from login.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "login.top.no" -Filter "*" -Properties memberOf,givenName,sn,displayName,employeeNumber,extensionAttribute4,extensionAttribute6,extensionAttribute7,company,physicalDeliveryOfficeName,mail,proxyAddresses,title,state -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"login"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, extensionAttribute7, @{N="departmentShort"; E={$_.extensionAttribute6}}, @{N="departmentAdditional"; E={$_.extensionAttribute4}}, @{N="office"; E={$_.physicalDeliveryOfficeName}}, @{N="company"; E={$_.company}}, title, state, @{N="feide"; E={if ($_.memberOf -like "*VT-ALLE-LÆRERE*") {$True} else {$False}}}
-Write-Host "Finding 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
-Write-Host "Finding apprentice 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS APPRENTICE" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
-Write-Host "Finding OT 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
-$adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS OT" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}
+    # get ad apprentice users from AUTO USERS
+    Write-Host "Finding apprentice 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "Title -eq 'Lærling'" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses,title -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={$_.title.ToLower()}}
+
+    # get ad ot kids from AUTO USERS
+    Write-Host "Finding OT 'AUTO USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS OT" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={"elev-ot"}}
+
+    # get ad users from AUTO DISABLED USERS
+    Write-Host "Finding 'AUTO DISABLED USERS' from login.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "login.top.no" -Filter "*" -Properties memberOf,givenName,sn,displayName,employeeNumber,extensionAttribute4,extensionAttribute6,extensionAttribute7,company,physicalDeliveryOfficeName,mail,proxyAddresses,title,state -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"login"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, extensionAttribute7, @{N="departmentShort"; E={$_.extensionAttribute6}}, @{N="departmentAdditional"; E={$_.extensionAttribute4}}, @{N="office"; E={$_.physicalDeliveryOfficeName}}, @{N="company"; E={$_.company}}, title, state, @{N="feide"; E={if ($_.memberOf -like "*VT-ALLE-LÆRERE*") {$True} else {$False}}}
+    Write-Host "Finding 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "Title -eq 'Elev'" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses,title -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={$_.title.ToLower()}}
+    
+    # get ad apprentice users from AUTO DISABLED USERS
+    Write-Host "Finding apprentice 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "Title -eq 'Lærling'" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses,title -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={$_.title.ToLower()}}
+    
+    # get ad ot kids from AUTO DISABLED USERS
+    Write-Host "Finding OT 'AUTO DISABLED USERS' from skole.top.no : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+    $adUsers += D:\Scripts\VTFK-Toolbox\AD\Get-VTFKADUser.ps1 -Domain "skole.top.no" -Filter "*" -Base "USERS OT" -Properties givenName,sn,displayName,employeeNumber,department,company,mail,proxyAddresses -OnlyDisabledAutoUsers | Select-Object userPrincipalName, samAccountName, givenName, mail, proxyAddresses, @{N="surName"; E={$_.sn}}, displayName, @{N="domain"; E={"skole"}}, employeeNumber, @{N="timestamp"; E={Get-Date -Format o}}, enabled, @{N="ou"; E={"AUTO DISABLED USERS"}}, @{N="departmentShort"; E={$_.department}}, @{N="office"; E={$_.company}}, @{N="company"; E={$_.company}}, @{N="type"; E={"elev-ot"}}
+
+    # export users
+    $exportFile = "$dbUpdateFolder\data\users.json"
+    Write-Host "Exporting data to '$($exportFile)'" -Verbose
+    $adUsers | ConvertTo-Json -Depth 20 | Out-File -FilePath $exportFile -Encoding utf8 -Force -NoNewline
+    
+    # update db
+    $currentLocation = Get-Location | Select-Object -ExpandProperty Path
+    try {
+        Set-Location -Path $dbUpdateFolder
+        Write-Host "Invoking node to update database" -Verbose
+        Invoke-Expression -Command "node .\index.js users" -ErrorAction Stop
+    }
+    catch {
+        Write-Error -Message "Failed to update DUST users DB : $_" -ErrorAction Stop
+    }
+    finally {
+        Set-Location -Path $currentLocation
+    }
+
+    Write-Host "DONE AD Users : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+}
+
+# import environment variables
+$envPath = Join-Path -Path $PSScriptRoot -ChildPath "..\envs.ps1"
+. $envPath
 
 # db-update folder path
 $dbUpdateFolder = Resolve-Path -Path "$($PSScriptRoot)\..\node\db-update" | Select-Object -ExpandProperty Path
 
-# export users
-$exportFile = "$dbUpdateFolder\data\users.json"
-Write-Host "Exporting data to '$($exportFile)'" -Verbose
-$adUsers | ConvertTo-Json -Depth 20 | Out-File -FilePath $exportFile -Encoding utf8 -Force -NoNewline
-
-# update db
-$currentLocation = Get-Location | Select-Object -ExpandProperty Path
-try {
-    Set-Location -Path $dbUpdateFolder
-    Write-Host "Invoking node to update database" -Verbose
-    Invoke-Expression -Command "node .\index.js users" -ErrorAction Stop
-}
-catch {
-    Write-Error -Message "Failed to update DUST users DB : $_" -ErrorAction Stop
-}
-finally {
-    Set-Location -Path $currentLocation
-}
-
-Write-Host "DONE : $(Get-Date -Format 'HH:mm:ss')" -Verbose
+# call update functions for AD users
+Update-DUSTADUsers
