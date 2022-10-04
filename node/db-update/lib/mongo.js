9cee4585fc253b2a93a25e18e3e5315e0103d6d2
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-10-04 13:22:45 +0200

    `isConnected` was removed in v4 so a new client was inited for every call 😬

--- node/db-update/lib/mongo.js
+++ node/db-update/lib/mongo.js
@@ -4,33 +4,18 @@ const { MONGODB_CONNECTION } = config
 
 let client = null
 
-module.exports = function getMongoDb (type) {
+module.exports = type => {
   if (!MONGODB_CONNECTION) {
     console.error('mongo', 'missing MONGODB_CONNECTION')
     throw new Error('Missing env MONGODB_CONNECTION')
   }
-  if (client && !client.isConnected) {
-    client = null
-    console.log('mongo', 'discard client')
-  }
+
   if (client === null) {
-    client = new MongoClient(MONGODB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    client = new MongoClient(MONGODB_CONNECTION)
     console.log('mongo', 'new client init')
-  } else if (client.isConnected) {
-    console.log('mongo', 'client connected', 'quick return')
-    return client.db(config[`MONGODB_${type.toUpperCase()}_NAME`]).collection(config[`MONGODB_${type.toUpperCase()}_COLLECTION`])
+  } else {
+    console.log('mongo', 'client already exists. quick return')
   }
 
-  return new Promise((resolve, reject) => {
-    client.connect(error => {
-      if (error) {
-        client = null
-        console.error('mongo', 'client error', error)
-        return reject(error)
-      } else {
-        console.log('mongo', 'new client connected')
-        resolve(client.db(config[`MONGODB_${type.toUpperCase()}_NAME`]).collection(config[`MONGODB_${type.toUpperCase()}_COLLECTION`]))
-      }
-    })
-  })
+  return client.db(config[`MONGODB_${type.toUpperCase()}_NAME`]).collection(config[`MONGODB_${type.toUpperCase()}_COLLECTION`])
 }
